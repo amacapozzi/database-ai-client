daf5136be8b04939feb826fc1d8816feb2268fe7
amacapozzi <mosqueracapozzia@gmail.com>
2025-09-06 12:17:31 -0300

    Refactor ConnectionString component to accept handleConnectDatabase prop and implement database connection logic

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,17 @@
 import { ConnectionString } from "@/components/connection-string";
+import { ConnectDatabase, GetTables } from "../wailsjs/go/main/App";
 
 function App() {
+  const handleConnectDatabase = async (url: string) => {
+    const database = await ConnectDatabase(url);
+    console.log(database);
+    const tables = await GetTables();
+    console.log(tables);
+  };
+
   return (
     <>
-      <ConnectionString />
+      <ConnectionString handleConnectDatabase={handleConnectDatabase} />
     </>
   );
 }
--- frontend/src/components/connection-string.tsx
+++ frontend/src/components/connection-string.tsx
@@ -1,37 +1,32 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
+import { useState } from "react";
 
-export function ConnectionString() {
+interface ConnectionProps {
+  handleConnectDatabase: (url: string) => void;
+}
+
+export function ConnectionString({ handleConnectDatabase }: ConnectionProps) {
+  const [databaseUrl, setDatabaseUrl] = useState("");
+
+  const handleConnect = () => {
+    handleConnectDatabase(databaseUrl);
+  };
+
   return (
     <div className={`w-full px-4 my-4`}>
       <div className="flex">
-        <Select>
-          <SelectTrigger className="w-max rounded-none rounded-l-lg border-r-neutral-800 focus:z-10">
-            <SelectValue />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem value="GET">GET</SelectItem>
-            <SelectItem value="POST">POST</SelectItem>
-            <SelectItem value="PUT">PUT</SelectItem>
-            <SelectItem value="DELETE">DELETE</SelectItem>
-            <SelectItem value="PATCH">PATCH</SelectItem>
-          </SelectContent>
-        </Select>
-
         <Input
+          onChange={(e) => setDatabaseUrl(e.target.value)}
           type="text"
           placeholder="Database URL"
-          className="flex-1 rounded-none border-x-0 focus:z-10 focus:outline-none focus:ring-0 focus:ring-offset-0 focus:shadow-none"
+          className="flex-1 rounded-none rounded-l-lg focus:z-10 focus:outline-none focus:ring-0 focus:ring-offset-0 focus:shadow-none"
         />
 
-        <Button className="rounded-l-none font-geist-mono bg-orange-600 hover:bg-orange-700 text-white px-6">
+        <Button
+          onClick={handleConnect}
+          className="rounded-l-none font-geist-mono bg-orange-600 hover:bg-orange-700 text-white px-6"
+        >
           Create Connection
         </Button>
       </div>
