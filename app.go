aed25250a2686ccf8f2e54a0486af54a1cfdc238
amacapozzi <mosqueracapozzia@gmail.com>
2025-09-06 12:17:01 -0300

    Refactor App struct to initialize DatabaseService and add database connection methods

--- app.go
+++ app.go
@@ -2,23 +2,37 @@ package main
 
 import (
 	"context"
+	"database-ai-client/services"
 	"fmt"
 )
 
 // App struct
 type App struct {
 	ctx context.Context
+	db  *services.DatabaseService
 }
 
 // NewApp creates a new App application struct
 func NewApp() *App {
-	return &App{}
+	return &App{
+		db: &services.DatabaseService{},
+	}
+
 }
 
 // startup is called when the app starts. The context is saved
 // so we can call the runtime methods
 func (a *App) startup(ctx context.Context) {
 	a.ctx = ctx
+}
+
+func (a *App) ConnectDatabase(databaseUrl string) error {
+	return a.db.Connect(databaseUrl)
+
+}
+
+func (a *App) GetTables() ([]string, error) {
+	return a.db.GetDatabaseTables()
 }
 
 // Greet returns a greeting for the given name
